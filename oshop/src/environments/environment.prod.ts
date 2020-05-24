12f1bc4c7022c1aeae7d0eba647ef22e526b9403
Diana Lim <lim.diana@outlook.com>
2020-05-23 20:45:14 -0700

    Added Firebase to project

--- oshop/src/environments/environment.prod.ts
+++ oshop/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBc_J4TzaBgzdN3mZ8P036p5zYKFiXAdcY",
+    authDomain: "oshop-b539f.firebaseapp.com",
+    databaseURL: "https://oshop-b539f.firebaseio.com",
+    projectId: "oshop-b539f",
+    storageBucket: "oshop-b539f.appspot.com",
+    messagingSenderId: "28209315160",
+    appId: "1:28209315160:web:181b2c43430b075a7f792d",
+    measurementId: "G-FZ4ZXMMDE5"
+  }
 };
--- oshop/src/environments/environment.ts
+++ oshop/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBc_J4TzaBgzdN3mZ8P036p5zYKFiXAdcY",
+    authDomain: "oshop-b539f.firebaseapp.com",
+    databaseURL: "https://oshop-b539f.firebaseio.com",
+    projectId: "oshop-b539f",
+    storageBucket: "oshop-b539f.appspot.com",
+    messagingSenderId: "28209315160",
+    appId: "1:28209315160:web:181b2c43430b075a7f792d",
+    measurementId: "G-FZ4ZXMMDE5"
+  }
 };
 
 /*
